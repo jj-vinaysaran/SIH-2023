468b57797a348017e35c6ce0f35ab979201ededc
trojanchall1 <mailtopavankalyan10@gmail.com>
2023-09-10 18:43:14 +0530

    hello

--- frontend/construction_x/src/Pages/Visual/Visual.jsx
+++ frontend/construction_x/src/Pages/Visual/Visual.jsx
@@ -3,8 +3,32 @@ import './visual.css';
 const Visual = () => {
   return (
     <div>
-        <h1>Visual</h1>
+      <div className='visual-nav'>
+        VISUALIZE YOUR HOME
+      </div>
+      <div className='visual-container'>
+        <div className='visual-left'>
+          
+          <div className='visual-left-upload'>
+            <h1 className='vis-upload-text'>UPLOAD YOUR 3D MODEL</h1>
+            <button className='vis-upload'>UPLOAD</button>
+          </div>
+          <div className='visual-left-compare'>
+            <h1 className='vis-compare-text'>COMPARE WORKFLOW</h1>
+            <button className='vis-compare'>COMPARE</button>
+          </div>
+          <div className='visual-left-rtm'>
+          <h1 className='vis-rtm-text'>MONITOR WORK</h1>
+            <button className='vis-rtm'>MONITOR</button>
+          </div>
+        </div>
+        <div className='visual-right'>
+
+        </div>
+      </div>
+
     </div>
+
   )
 }
 
