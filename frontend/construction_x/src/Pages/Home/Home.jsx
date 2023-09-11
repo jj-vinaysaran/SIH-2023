ca4fb7717d27c63e48d429c82973ceaf973dd5f0
Sameerul-Hak <sameerulhakofficial@gmail.com>
2023-09-11 11:46:37 +0530

    final

--- frontend/construction_x/src/Pages/Home/Home.jsx
+++ frontend/construction_x/src/Pages/Home/Home.jsx
@@ -18,35 +18,37 @@ const Home = () => {
           </div >
           <div className='labours'>
             <img src={labour}/>
-            <h2 ><Link to="/labour" className='link'>Labours</Link></h2>
+            <h2 ><Link to="/labour" className='link'>LABOURS</Link></h2>
           </div>
           <div className='labours'>
             <img src={support}/>
-            <h2 ><Link className="link" to='/material'>Materials</Link></h2>
+            <h2 ><Link className="link" to='/material'>MATERIALS</Link></h2>
           </div>
           <div className='labours'>
             <img src={graph}/>
-            <h2 ><Link className="link" to="/visual">Visualize</Link></h2>
+            <h2 ><Link className="link" to="/visual">VISUALIZE</Link></h2>
           </div>
-          <div className='labours'>
-            <img src={safety}/>
-            <h2 ><Link className="link" to="/safety">Safety</Link></h2>
-          </div>
+          
           <div className='labours'>
             <img src={support}/>
-            <h2 ><Link className="link" to='/support'>Support</Link></h2>
-          </div>
-          <div className='labours'>
-            <img src={machines}/>
-            <h2 ><Link className="link">maintainence</Link></h2>
+            <h2 ><a href='https://constructionx.netlify.app/support' className="link" to='/support'>SUPPORT</a></h2>
           </div>
           <div className='labours'>
             <img src={energy}/>
-            <h2 ><Link className="link">Energy</Link></h2>
+            <h2 ><a  href="https://constructionx.netlify.app/energy" className="link">ENERGY</a></h2>
           </div>
           <div className='labours'>
+            <img src={machines}/>
+            <h2 ><a href='https://www.iirs.gov.in/construction-and-maintenance-division' className="link">MAINTENANCE</a></h2>
+          </div>
+         
+          <div className='labours'>
             <img src={support}/>
-            <h2 ><Link className="link">Budget</Link></h2>
+            <h2 ><Link className="link">BUDGET</Link></h2>
+          </div>
+          <div className='labours'>
+            <img src={safety}/>
+            <h2 ><Link className="link" to="/safety">SAFETY</Link></h2>
           </div>
           
           
@@ -57,12 +59,11 @@ const Home = () => {
           <h3 className='logtext' id='logout'>Logout</h3>
         </div>
        
-        <img src="https://images.squarespace-cdn.com/content/v1/5cf799216e35c10001a486eb/9122ca30-0e5e-4179-8c8d-cbe1bde5dcde/Screen+Recording+2022-02-02+at+9.38.31+AM.mov-high.gif" className="threed"/>
 
        
         <div className='main_three'>
             <div className='b1' id='madhankonar'>
-              <h2 className='labtext'>Labour Line</h2>
+              <h2 className='labtext'>LABOUR LINE</h2>
               <div class='message'>
                             <div class="msg_row">
                                 <img src={act} class="act_img"/>
@@ -83,7 +84,7 @@ const Home = () => {
               </div>
             </div>
             <div className='b1'>
-              <h2 className='wt' >WareHouse</h2>
+              <h2 className='wt' >WAREHOUSE</h2>
               <div className='ware'>
                 <h3>Available Cement-<span className='st'>20 units</span> </h3>
                 <h3>Available sand- <span className='st'>30 units</span></h3>
@@ -92,11 +93,16 @@ const Home = () => {
               </div>
             </div>
             <div className='b1'>
-              <h2 className='buton'>Upload your Model</h2>
-              <input type='file' className='inp' />
+            <div className='labour_attendance'>
+              <h2  className='wt'>LABOUR ATTENDANCE</h2>
+              <h4  id='lab_id'>Present :<span>1810</span> </h4>
+              <h4  id='lab_id'>Absent: <span>210</span></h4>
+              <h4 id='lab_id'>Half-duty : <span>55</span></h4>
+            </div>
             </div>
         </div>
 
+      <img src="https://images.squarespace-cdn.com/content/v1/5cf799216e35c10001a486eb/9122ca30-0e5e-4179-8c8d-cbe1bde5dcde/Screen+Recording+2022-02-02+at+9.38.31+AM.mov-high.gif" className="threed"/>
       </div>
     </div>
   )
--- frontend/construction_x/src/Pages/Labour/Labour.jsx
+++ frontend/construction_x/src/Pages/Labour/Labour.jsx
@@ -1,11 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './labour.css';
+import jpg from '../../images/cmp.jpg';
+import labi from "../../images/lab.jpeg"
 const Labour = () => {
-  return (
-    <div>
-        <h1>Labour</h1>
-    </div>
-  )
-}
+  const [selectedSector, setSelectedSector] = useState('');
+  const [selectedSkill, setSelectedSkill] = useState('');
+  const [labourCount, setLabourCount] = useState(0);
+  const [selectedTask, setSelectedTask] = useState(''); // New state for selected task
+  const [allocationMessage, setAllocationMessage] = useState('');
 
-export default Labour;
+  const handleSectorChange = (event) => {
+    setSelectedSector(event.target.value);
+  };
+
+  const handleSkillChange = (event) => {
+    setSelectedSkill(event.target.value);
+  };
+
+  const handleTaskChange = (event) => {
+    setSelectedTask(event.target.value);
+  };
+
+  const handleAllocateClick = () => {
+    // Perform the allocation logic here, based on the selected task and sector.
+    // For example, you can show a success message.
+    const message = `Task "${selectedTask}" allocated to Sector "${selectedSector}"`;
+    setAllocationMessage(message);
+  };
+   const handleSearchClick = () => {
+    // Here you can implement your logic to count the labor based on the selected sector and skill.
+    // For now, I'm setting a random count as an example.
+    const randomCount = Math.floor(Math.random() * 100); // Replace with your actual logic.
+    setLabourCount(randomCount);
+  };
+
+  const mapSrc = jpg;
+
+  return (
+    <div className='cont'>
+      <div className='labour_nav'>
+        <h1 className='labour_head'>Labour</h1>
+      </div>
+      <div className='labour_bg'>
+        <div className='labour_container'>
+          <div className='labour_left'>
+          <div className='labour_attendance'>
+              <h3 className='labour_sub'>Labour attendance</h3>
+              <h4 className='labour_txt'>Present :<span>1810</span> </h4>
+              <h4 className='labour_txt'>Absent: <span>210</span></h4>
+              <h4 className='labour_txt'>Half-duty : <span>55</span></h4>
+              <button className='labour_btn'>Enter Attendance</button>
+            </div>
+            <div className='labour_search'>
+              <h3 className='labour_sub'>Labour Search</h3>
+              <button>Search</button>
+            
+            </div>
+            <div className='labour_task'>
+              <h3 className='labour_sub'>Labour Task</h3>
+              
+              <button>Allocate</button>
+             
+            </div>
+            <div className='labour_task'>
+              <h3 className='labour_sub'>Report Generator</h3>
+              
+              <button>Generate Report</button>
+             
+            </div>
+          
+          </div>
+          <div className='labour_right'>
+            <h3 className='labour_sub'>Blueprint Construction Map</h3>
+            <img src={labi} className='labour_map'/>
+            {/* <iframe
+              src={mapSrc}
+              title='Blueprint Map'
+              className='labour_map'
+              width='100%'
+              height='100%'
+              frameBorder='0'
+            ></iframe> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Labour;  
